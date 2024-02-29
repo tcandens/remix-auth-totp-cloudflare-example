76e30ee616c7f803fdf0867ec515ca08cd99b7a9
Joey Thies <joey.thies@gmail.com>
2024-02-28 16:28:32 -0800

    canary react

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+  plugins: [
+    remixCloudflareDevProxy(), 
+    remix(), 
+    tsconfigPaths()
+  ],
 });
